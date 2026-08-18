17fbc10f9f0dd2036229983b62af1c8f2b165ae2
AngeloC3 <angelocataldo33@gmail.com>
2026-08-18 15:46:21 -0400

    reversion to concrete media item

--- lib/domain/media/media_item.dart
+++ lib/domain/media/media_item.dart
@@ -2,7 +2,7 @@ import 'package:cybershelf/domain/media_type.dart';
 import 'package:cybershelf/domain/media/media_metadata.dart';
 import 'package:cybershelf/domain/media/media_user_data.dart';
 
-abstract class MediaItem {
+class MediaItem {
   const MediaItem({
     required this.id,
     required this.type,
@@ -14,4 +14,18 @@ abstract class MediaItem {
   final MediaType type;
   final MediaMetadata metadata;
   final MediaUserData userData;
+
+  MediaItem copyWith({
+    int? id,
+    MediaType? type,
+    MediaMetadata? metadata,
+    MediaUserData? userData,
+  }) {
+    return MediaItem(
+      id: id ?? this.id,
+      type: type ?? this.type,
+      metadata: metadata ?? this.metadata,
+      userData: userData ?? this.userData,
+    );
+  }
 }
